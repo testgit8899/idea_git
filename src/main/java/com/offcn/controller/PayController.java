d16a973bcd7c8341729ab8cf1f5231c644f35d6a
wyan <121757925@qq.com>
2020-12-14 17:09:46 +0800

    右侧记录姓名ip

--- src/main/java/com/offcn/controller/PayController.java
+++ src/main/java/com/offcn/controller/PayController.java
@@ -1,11 +1,17 @@
 package com.offcn.controller;
 
+import java.util.Date;
+
 /**
  * @Auther: wyan
  * @Date: 2020-12-14 16:46
  * @Description:
  */
 public class PayController {
+
+    private static String hostIp;
+    private static String userName;
+
     public static void main(String[] args) {
         long time1 = new Date().getTime();
         for(int i=0;i<100;i++){
@@ -13,11 +19,11 @@ public class PayController {
         }
         long time2 = new Date().getTime();
         System.out.println(time2-time1);
-    }
-    private static String hostIp;
-    private static String userName;
-
-    public static void main(String[] args) {
         System.out.println("当前访问的ip=="+hostIp+"----"+userName);
     }
+
+
+
+
+
 }
