869bdfcb604fa525d41d228ac41f00ae29ccc43c
wyan <121757925@qq.com>
2020-12-14 16:53:05 +0800

    新加的原来代码

---
+++ src/main/java/com/offcn/controller/OldController.java
@@ -0,0 +1,13 @@
+package com.offcn.controller;
+
+/**
+ * @Auther: wyan
+ * @Date: 2020-12-14 16:52
+ * @Description:
+ */
+public class OldController {
+
+    public static void main(String[] args) {
+        System.out.println("我是原来的工程");
+    }
+}
