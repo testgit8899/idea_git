77ac4284ba1fd0a142770ffe7438436babed9d59
wyan <121757925@qq.com>
2020-12-14 17:04:33 +0800

    监控代码

--- src/main/java/com/offcn/controller/PayController.java
+++ src/main/java/com/offcn/controller/PayController.java
@@ -1,9 +1,19 @@
 package com.offcn.controller;
 
+import java.util.Date;
+
 /**
  * @Auther: wyan
  * @Date: 2020-12-14 16:46
  * @Description:
  */
 public class PayController {
+    public static void main(String[] args) {
+        long time1 = new Date().getTime();
+        for(int i=0;i<100;i++){
+            System.out.println(i);
+        }
+        long time2 = new Date().getTime();
+        System.out.println(time2-time1);
+    }
 }
