79961477726f9e642448e1e6a587e112396f8337
wyan <121757925@qq.com>
2020-12-14 17:04:48 +0800

    右侧记录姓名ip

--- src/main/java/com/offcn/controller/PayController.java
+++ src/main/java/com/offcn/controller/PayController.java
@@ -6,4 +6,10 @@ package com.offcn.controller;
  * @Description:
  */
 public class PayController {
+    private static String hostIp;
+    private static String userName;
+
+    public static void main(String[] args) {
+        System.out.println("当前访问的ip=="+hostIp+"----"+userName);
+    }
 }
